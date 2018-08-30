d99f58bb384851a39e6d64bd11126b8b1d59ed7f
ZWVova <zwvova@yandex.ua>
2018-08-30 22:54:50 +0300

    Оптимизировали фиксиуру, тесты выполняются в одной сессии

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,14 +3,10 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="qwe", middlename="qwert", lastname="qwerty", nickname="qaz",
                              photo="C:\\Users\\Vova\\Desktop\\qaz.png", home="123456", email="qwe@qwe.qw"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
                              photo="", home="", email=""))
-    app.session.logout()
