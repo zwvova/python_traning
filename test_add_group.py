0399dc7ba6bc741aa03f230af98951b8375cbd1c
ZWVova <zwvova@yandex.ua>
2018-08-30 13:18:50 +0300

    2_1 driver

--- test_add_group.py
+++ test_add_group.py
@@ -18,20 +18,14 @@ class test_add_group(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="qaz", header="qaz", footer="qaz"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
 
@@ -42,6 +36,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
+        self.open_group_page(wd)
         # init groop creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -56,11 +51,13 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def open_group_page(self, wd):
         wd.find_element_by_link_text("Групи").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
