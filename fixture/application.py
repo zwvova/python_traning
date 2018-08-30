49b93af14db52e672945ea8e17a65bb55486ffbd
ZWVova <zwvova@yandex.ua>
2018-08-30 23:38:08 +0300

    Интелектуальная фикстура. Добавлена проверка в фикстуру.

--- fixture/application.py
+++ fixture/application.py
@@ -8,10 +8,17 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
 
     def open_home_page(self):
         wd = self.wd
