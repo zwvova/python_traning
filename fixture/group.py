0d0eceae684969c249c9db6848c9dea43af4b0da
ZWVova <zwvova@yandex.ua>
2018-08-31 16:53:33 +0300

    Loading list. Assert

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,6 @@
+from model.group import Group
+
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -63,3 +66,13 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_group_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,14 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="qaz", header="qaz", footer="qaz"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
