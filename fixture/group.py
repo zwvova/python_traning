8667ef09b5bbb83bdd9e95103b4f005773e99edc
ZWVova <zwvova@yandex.ua>
2018-08-31 19:11:45 +0300

    Оптимизация - кеширование

--- fixture/group.py
+++ fixture/group.py
@@ -20,6 +20,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cashe = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -41,6 +42,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cashe = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -57,6 +59,7 @@ class GroupHelper:
         # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cashe = None
 
     def return_to_groups_page(self):
         wd = self.app.wd
@@ -67,12 +70,15 @@ class GroupHelper:
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cashe = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_group_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cashe is None:
+            wd = self.app.wd
+            self.open_group_page()
+            self.group_cashe = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cashe.append(Group(name=text, id=id))
+        return list(self.group_cashe)
