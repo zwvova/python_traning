c485210045e52bfd8d8917e27f498a43323dceb7
ZWVova <zwvova@yandex.ua>
2018-08-31 18:02:12 +0300

    Sorting list v2

--- model/group.py
+++ model/group.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Group:
 
     def __init__(self, name=None, header=None, footer=None, id=None):
@@ -11,3 +14,9 @@ class Group:
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
