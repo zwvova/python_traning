60e99b6440f7cd00d2e7c49ce25744cb337e115d
ZWVova <zwvova@yandex.ua>
2018-08-31 12:47:26 +0300

    Optimization
    1. оптимизированы переходы по страницам
    2. удалена задержка, так как сайт не динамический

--- fixture/application.py
+++ fixture/application.py
@@ -8,7 +8,6 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox(capabilities={"marionette": False})
-        self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
