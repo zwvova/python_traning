1710218d6e0f4845841bfd3d25fbcee7e4883599
ZWVova <zwvova@yandex.ua>
2018-09-01 23:55:53 +0300

    Добавлен тест сравнения телефонов на главной и странице редактирования

--- fixture/contact.py
+++ fixture/contact.py
@@ -24,7 +24,7 @@ class ContactHelper:
         self.change_field_value("home", contact.homephone)
         self.change_field_value("mobile", contact.mobilephone)
         self.change_field_value("work", contact.workphone)
-        self.change_field_value("fax", contact.secondaryphone)
+        self.change_field_value("phone2", contact.secondaryphone)
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -41,11 +41,14 @@ class ContactHelper:
             self.app.open_home_page()
             self.contact_cashe = []
             for row in wd.find_elements_by_name("entry"):
-                cells = row.find_element_by_tag_name("td")
+                cells = row.find_elements_by_tag_name("td")
                 firstname = cells[1].text
                 lastname = cells[2].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                self.contact_cashe.append(Contact(firstname=firstname, lastname=lastname, id=id))
+                all_phones = cells[5].text.splitlines()
+                self.contact_cashe.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  homephone=all_phones[0], mobilephone=all_phones[1],
+                                                  workphone=all_phones[2], secondaryphone=all_phones[3]))
         return list(self.contact_cashe)
 
     def open_contact_to_edit_by_index(self, index):
@@ -61,3 +64,16 @@ class ContactHelper:
         row = wd.find_elements_by_name("entry")[index]
         cell = row.find_elements_by_tag_name("td")[6]
         cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id,
+                       homephone=homephone, workphone=workphone, mobilephone=mobilephone, secondaryphone=secondaryphone)
---
+++ test/test_phones.py
@@ -0,0 +1,13 @@
+import re
+
+def test_phones_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.homephone == clear(contact_from_edit_page.homephone)
+    assert contact_from_home_page.workphone == clear(contact_from_edit_page.workphone)
+    assert contact_from_home_page.mobilephone == clear(contact_from_edit_page.mobilephone)
+    assert contact_from_home_page.secondaryphone == clear(contact_from_edit_page.secondaryphone)
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
