6b1a0c3fbedd4f9ef6baf9bfcd38a36bcb91bb3a
ZWVova <zwvova@yandex.ua>
2018-09-02 20:23:07 +0300

    Создание параметризованого теста.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
+import pytest
 from model.group import Group
 
 
-def test_add_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="qaz", header="qaz", footer="qaz")
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+testdata = [
+    Group(name="qaz", header="qaz", footer="qaz"),
+    Group(name="", header="", footer="")
+]
 
 
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
+    pass
+    # old_groups = app.group.get_group_list()
+    # app.group.create(group)
+    # assert len(old_groups) + 1 == app.group.count()
+    # new_groups = app.group.get_group_list()
+    # old_groups.append(group)
+    # assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    #
+    #
