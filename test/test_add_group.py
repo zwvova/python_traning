6750b5d0bd6558abb352eff47cbcdf589b72c916
ZWVova <zwvova@yandex.ua>
2018-09-02 20:49:06 +0300

    Полный перебор данных с пустыми строками

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,9 +10,11 @@ def random_string(prefix, maxlen):
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(5)
+testdata = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 20)]
+    for footer in ["", random_string("footer", 20)]
 ]
 
 
