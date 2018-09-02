9e912b1b98b13abe3f06f97c0a8b1fe6812518af
ZWVova <zwvova@yandex.ua>
2018-09-02 23:13:20 +0300

    Доработан код с параметризацией в фикстуре

--- conftest.py
+++ conftest.py
@@ -8,13 +8,13 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--base_url")
     if fixture is None:
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--base_url")
-        fixture = Application(browser=browser, base_url=base_url)
+            fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
