928e24e1b78542b54810b212daf759183c9bbf10
ZWVova <zwvova@yandex.ua>
2018-09-02 20:03:13 +0300

    Добавлен тест методом обратных проверок

--- model/contact.py
+++ model/contact.py
@@ -3,8 +3,8 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None,
-                 workphone=None, secondaryphone=None, id=None):
+    def __init__(self, firstname=None, lastname=None, id=None, homephone=None, mobilephone=None,
+                 workphone=None, secondaryphone=None, all_phones_from_home_page=None):
         self.firstname = firstname
         self.lastname = lastname
         self.homephone = homephone
@@ -12,6 +12,7 @@ class Contact:
         self.workphone = workphone
         self.secondaryphone = secondaryphone
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
