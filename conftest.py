c0244a61550f8d4522302ffe32ab85b4a8ae2641
ZWVova <zwvova@yandex.ua>
2018-09-02 23:05:38 +0300

    Добавлен параметр для запуска из командной строки
    base_url

--- conftest.py
+++ conftest.py
@@ -10,7 +10,8 @@ def app(request):
     global fixture
     if fixture is None:
         browser = request.config.getoption("--browser")
-        fixture = Application(browser=browser)
+        base_url = request.config.getoption("--base_url")
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
             fixture = Application()
@@ -29,3 +30,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--base_url", action="store", default="http://addressbook/")
--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,7 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self, browser="firefox"):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox(capabilities={"marionette": False})
         elif browser == "chrome":
@@ -18,6 +18,7 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -28,7 +29,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
