87a98669cee2f37d15f25c3b15f498bd6d20eb0d
ZWVova <zwvova@yandex.ua>
2018-09-03 12:27:32 +0300

    Прописан конфигурационный файл

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import json
+import os.path
 from fixture.application import Application
 
 
@@ -12,8 +13,9 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
             fixture = Application(browser=browser, base_url=target['baseUrl'])
     fixture.session.ensure_login(username=target["username"], password=target["password"])
@@ -32,3 +34,12 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+
+
+"""config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+    __file__ - содержит информацию, путь к текущему файлу,
+    так как путь м.б. указать как абсолютный или как относительный, то проводим преобразования.
+    преобразуем путь в абсолютный os.path.abspath
+    определяем директорию в которой файл находится os.path.dirname
+    к данной директории прикрепляем файл с конфигурационными данными, который указан относительно нее os.path.join
+"""
